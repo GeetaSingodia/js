0d7472362d5edf7c6001df18cc61064888b5b706
geeta <g_singodia@yahoo.com>
2024-05-21 16:47:44 -0400

    Alab30811

---
+++ Alab30811.js
@@ -0,0 +1,97 @@
+// Inisialize 5 numbers add upto 50
+// let n1 = 20;
+// let n2 = 15;
+// let n3 = 5;
+// let n4 = 10;
+// // checking number is divisible by 5
+// let r1 = n1 ==(n1/5) ? true : false ;
+// console.log(r1) ;
+// let r2 = n2 ==(n2/5) ? true : false ;
+// console.log(r2);
+// let r3 = n3 ==(n3/5) ? true : false ;
+// console.log(r3);
+// let r4 = n4 ==(n4/5) ? true : false ;
+// console.log(r4);
+
+
+// // check first number is larger than last
+ 
+//  if (n1 > n4) {
+//     console.log("n1 is greater") ;
+//  }else {
+//     console.log("n4 is greater") ;
+//  }
+
+//  // Subtract 1st number from second
+// let sub1 = (n1-n2) ;
+// console.log(sub1);
+// let mul1 = sub1*n3 ;
+// console.log(mul1);
+// let div1 = mul1/n4 ;
+// console.log(div1);
+
+// // is over 25
+
+// let result=!(n1 > 25 || n2 > 25 || n3 > 25 || n4 > 25)
+// log.console(result);
+
+
+
+// practical Math part 2
+//initialize the total distance
+// how much gallons of fuel required
+
+// solving first option
+let totaldis = 1500;
+let fuel1 = 30;
+
+let x =(totaldis/fuel1);
+console.log(x);
+
+// solving 2nd optiom
+//let totaldis1 = 1500;
+let fuel2 = 28 ;
+
+let y = (totaldis/fuel2);
+console.log(y);
+
+// solving 3rd option
+//let totaldis2 = 1500;
+let fuel3 = 23;
+
+let z = (totaldis/fuel3);
+console.log(z);
+
+// will budget be enough to cover fuel expenses
+// fuel per gallon is $3 so we multiply the answers of 1st equation to 3
+
+let c = 3 ;
+
+// calculate the fuel expenses of 1st option
+let tot = (c * x);
+console.log(tot);
+
+// calculates the second option
+let tot1 = (c * y);
+console.log(tot1);
+
+// calculation for 3rd option
+let tot2 = (c * z);
+console.log(tot2);
+
+// How long will trip take in hrs
+// finding the total hour for 1st option
+let hr1 = 55;
+let tothr1 = (totaldis/hr1);
+console.log(tothr1);
+
+// find the total hour for 2nd option
+let hr2 = 60;
+let tothr2 = (totaldis/hr2);
+console.log(tothr2);
+
+// find the total hour for 3rd option
+let hr3 = 75;
+let tothr3 = (totaldis/hr3);
+console.log(tothr3);
+
